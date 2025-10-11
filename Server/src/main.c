e893ad5962d4d531bfcc5cd6a31be534fd1a41ac
IsaacAmann <isaacmnn@gmail.com>
2025-10-11 00:24:07 -0500

    Linked mariadb connector and tested database connection

--- Server/src/main.c
+++ Server/src/main.c
@@ -1,6 +1,7 @@
 
 #include "main.h"
 #include "PlayerConnectionManager.h"
+#include <mysql.h>
 
 int isServerRunning;
 
@@ -18,7 +19,18 @@ int main()
 	//Global variable. Other threads should monitor this for signal to shutdown
 	isServerRunning = 1;
 	
+	//test database connection
+	MYSQL *connection;
+	connection = mysql_init(0);
+	
+	mysql_real_connect(connection, "localhost", "mmo_server", "password", "C_MMORPG", 3306, NULL, 0);
+	
+	printf("%s\n", mysql_error(connection));
+	
+	const char* serverVersion;
+	mariadb_get_infov(connection, MARIADB_CONNECTION_SERVER_VERSION, (void*)&serverVersion);
 
+	printf("%s\n", serverVersion);
 	
 	//Hold for threads to exit
 	pthread_join(playerConnectionManagerThread, NULL);
