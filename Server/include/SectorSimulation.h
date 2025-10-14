ff9556ff42639d3a24977322dfc5f1430c2d0290
IsaacAmann <isaacmnn@gmail.com>
2025-10-13 21:47:17 -0500

    More sector setup

--- Server/include/SectorSimulation.h
+++ Server/include/SectorSimulation.h
@@ -5,12 +5,33 @@
 #include <box2d/collision.h>
 #include <box2d/math_functions.h>
 #include <glib.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
 typedef struct Sector
 {
 	b2DynamicTree dynamicTree;
+	pthread_t simulationLoopThread;
+	int isRunning;
+	
 } Sector;
 
-void createSector(Sector *sector);
+//Create a new sector and store it in the address provided
+void createSector(Sector* sector);
+
+//Free sector memory
+void deleteSector(Sector* sector);
+
+//Add entity to the simulation. Can be any descendent of BaseEntity
+void addEntity(BaseEntity* entity);
+
+//Contains statements that should be run each step of the simulation
+void* playerConnectionThreadRun(void *args)
+
+
+
+
+
 
 #endif
